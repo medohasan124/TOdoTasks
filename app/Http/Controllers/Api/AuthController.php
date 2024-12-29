9d653409e34e9858ebea7c9bff0d4e356f667380
medo <medohasan124@gmail.com>
2024-12-30 01:00:30 +0300

    first Commit

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function login(Request $request){
+
+
+        $validate = Validator::make($request->all(),[
+            'email' => 'required' ,
+            'password' => 'required' ,
+        ]);
+
+         if($validate->fails()){
+            return response()->json($validate->errors());
+         }
+
+        $credintial = auth()->attempt([
+            'email' => $request->email,
+            'password' => $request->password
+        ]);
+
+
+        if($credintial){
+             $user = auth()->user() ;
+             $data['user'] = new UserResource($user);
+             $data['token'] = $user->createToken('Token')->plainTextToken;
+             return response()->json($data , 200);
+        }else{
+         return response()->json([],1,__('auth.failed'));
+        }
+
+    }
+
+
+
+    public function register(Request $request)
+    {
+        /*
+    register
+    1 - make validator name email password
+    2 if fails return error
+    3 merge password request
+    4 - create user at DB
+    5 create data and token
+    6 return reponse api
+    */
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->first(), 403);
+        }
+
+        $request->merge([
+            'password' => bcrypt($request->password),
+        ]);
+
+        $user = User::create($request->all());
+
+        $data['user'] = new UserResource($user);
+        $data['token'] = $user->createToken('registertoken')->plainTextToken;
+        return response()->json($data, 203);
+    }
+}
+
+
---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::post('register', [App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('login', [App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+   Route::resource('tasks', App\Http\Controllers\Api\TaskController::class);
+   Route::get('/tasks/{id}/share', [App\Http\Controllers\Api\TaskController::class, 'share']);
+   Route::post('/tasks/{id}/share', [App\Http\Controllers\Api\TaskController::class, 'share']);
+
+});
